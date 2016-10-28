1746c40f16fa0790f8c71190fa51c0e4b382df42
Connor Davidson <connordavidson93@gmail.com>
2016-10-28 11:59:17 -0600

    Levels, loops, randomized numbers

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@
 
     public class Program
     {
+        public static bool canPlay = true;
         public static Game myGame = new Game();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please type in your name.");
-            myGame.name = Console.ReadLine();
-            Console.WriteLine("Your Player Name is " + myGame.name);
             myGame.Start();
         }
+        
             
     }
--- scripts/Game.cs
+++ scripts/Game.cs
@@ -4,21 +4,27 @@ public class Game {
         Health.power = 100;
         Health.message = "You are getting stronger.";
         Ammo.message = "You have more ammo.";
+        Cave.StartMessage = "You have entered a cave.";
     }
 
+    //Runs at the start of the game
     public void Start(){
-        Health.RunPowerUp();
-        /*
-        After prompt the game for a name we:
-        Enter a cave.
-        Find Health.
-        Meet a dragon. (need an enemy class)
-        Pick a weapon.
-        Battle dragon. (need battle class)
-        If we win: get Health and ammo
-        If dragon wins: Lose Health;
-        */
+        Console.WriteLine("Please type in your name.");
+        name = Console.ReadLine();
+        Console.WriteLine("Your Player Name is " + name);
+       while(Program.canPlay){
+           Walk();
+       }
+       Console.WriteLine("You died.");
     }
+
+    private void Walk(){
+        Random randomNum = new Random();
+        Cave.Enter();
+        Cave.Encounter(randomNum.Next(0, Cave.objects.Length));
+    }
+    //Game Levels
+    private LevelBase Cave = new LevelBase();
 
     //Game PowerUps
     public PowerUpBase Health = new PowerUpBase();
@@ -29,6 +35,18 @@ public class Game {
     private WeaponBase Gun = new WeaponBase();
     private WeaponBase Rifle = new WeaponBase();
     private WeaponBase Knife = new WeaponBase();
+
+    
     public string name;
     private int score;
 }
+ /*
+        After prompt the game for a name we:
+        Enter a cave.
+        Find Health.
+        Meet a dragon. (need an enemy class)
+        Pick a weapon.
+        Battle dragon. (need battle class)
+        If we win: get Health and ammo
+        If dragon wins: Lose Health;
+        */
